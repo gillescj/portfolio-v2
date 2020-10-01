282f94d38df3e21f68ddc8efff2b965e3a45412d
Colin Gillespie <ratchetandcolin@gmail.com>
2020-10-01 18:57:06 -0400

    initial commit

---
+++ src/styling.js
@@ -0,0 +1,14 @@
+export const QUERIES = {
+    small: 'max-width: 600px',
+    medium: 'max-width: 850px',
+    large: 'max-width: 1100px',
+    xLarge: 'max-width: 1400px',
+    maxWidth: '1400px',
+    maxHeaderWidth: '1600px',
+};
+
+export const COLOURS = {
+    accent: 'hsl(187, 100%, 49%)',
+    textMain: 'white',
+    bgMain: 'black',
+};
