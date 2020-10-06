e6a823c71214f24c014bafe101ae7d6305698233
Colin Gillespie <ratchetandcolin@gmail.com>
2020-10-06 18:02:54 -0400

    Create project information data to draw from

---
+++ src/data/projectData.js
@@ -0,0 +1,80 @@
+export const projectDataList = [
+    {
+        title: `Gatsby's Pizzeria`,
+        skills: ['React', 'Gatsby', 'SCSS', 'Styled-Components'],
+        description: `A fake family pizzeria restaurant website built on GatsbyJS.`,
+        links: {
+            siteUrl: 'https://pizzeria.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/pizzeria',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/pizzeria-scroll.jpg?raw=true',
+    },
+    {
+        title: `Reddit Clone`,
+        skills: ['React', 'SCSS'],
+        description: `This reddit clone application displays posts and
+        subreddit communities within a theme that is
+        inspired by elements from both new and old reddit
+        websites.`,
+        links: {
+            siteUrl: 'https://reddit-clone.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/reddit-clone',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/reddit-clone-scroll.jpg?raw=true',
+    },
+    {
+        title: `Colour Builder`,
+        skills: ['React', 'SCSS', 'Styled-Components'],
+        description: `A tool designed to help developers select colours
+    and visualize the HSL (hue, saturation, lightness)
+    colour system. Name and save favourite colours for
+    future use.`,
+        links: {
+            siteUrl: 'https://colour-builder.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/colour-builder',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/colour-builder-scroll.png?raw=true',
+    },
+    {
+        title: `Nonstop News`,
+        skills: ['React', 'Redux', 'SCSS', 'Styled-Components'],
+        description: `Nonstop News displays news article headlines from
+    all over the web. Browse popular highlights by
+    country and category.`,
+        links: {
+            siteUrl: 'https://nonstop-news.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/nonstop-news',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/reddit-clone-scroll.jpg?raw=true',
+    },
+    {
+        title: `Game Atlas`,
+        skills: ['React', 'SCSS'],
+        description: `Game Atlas is a game database search application
+    for looking up your favourite games. This
+    application utilizes the IGDB API to receive and
+    inject data based off the user's search query.`,
+        links: {
+            siteUrl: 'https://game-atlas.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/game-atlas',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/reddit-clone-scroll.jpg?raw=true',
+    },
+    {
+        title: `Cryptosh`,
+        skills: ['JavaScript', 'SCSS', 'Webpack'],
+        description: `Cryptosh is the homepage showcase for the fake
+    e-commerce product "Cryptosh".`,
+        links: {
+            siteUrl: 'https://cryptosh.colingillespie.dev/',
+            githubUrl: 'https://github.com/gillescj/cryptosh',
+        },
+        image:
+            'https://github.com/gillescj/files/blob/master/reddit-clone-scroll.jpg?raw=true',
+    },
+];
