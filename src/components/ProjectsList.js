a55986db4aa26229cfba41aa58b6ec06000cd8b9
Colin Gillespie <ratchetandcolin@gmail.com>
2020-10-06 18:03:14 -0400

    Hook up Project components to project data

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ProjectSection from './ProjectSection';
+import { projectDataList } from 'data/projectData';
 
 const Container = styled.section`
     display: grid;
@@ -16,38 +17,12 @@ const ProjectsList = () => {
     return (
         <Container>
             <ProjectsHeader>Projects</ProjectsHeader>
-
-            <ProjectSection
-                content={{
-                    title: `Gatsby's Pizzeria`,
-                    skills: ['React', 'Gatsby', 'SCSS', 'Styled-Components'],
-                    description: `A fake family pizzeria restaurant website built on GatsbyJS.`,
-                    links: {
-                        siteUrl: 'https://pizzeria.colingillespie.dev/',
-                        githubUrl: 'https://github.com/gillescj/pizzeria',
-                    },
-                    image:
-                        'https://github.com/gillescj/files/blob/master/pizzeria-scroll.jpg?raw=true',
-                }}
-                placement="right"
-            />
-            <ProjectSection
-                content={{
-                    title: `Reddit Clone`,
-                    skills: ['React', 'SCSS'],
-                    description: `This reddit clone application displays posts and
-                        subreddit communities within a theme that is
-                        inspired by elements from both new and old reddit
-                        websites.`,
-                    links: {
-                        siteUrl: 'https://reddit-clone.colingillespie.dev/',
-                        githubUrl: 'https://github.com/gillescj/reddit-clone',
-                    },
-                    image:
-                        'https://github.com/gillescj/files/blob/master/reddit-clone-scroll.jpg?raw=true',
-                }}
-                placement="left"
-            />
+            {projectDataList.map((project, index) => (
+                <ProjectSection
+                    content={project}
+                    placement={index % 2 ? 'left' : 'right'}
+                />
+            ))}
         </Container>
     );
 };
