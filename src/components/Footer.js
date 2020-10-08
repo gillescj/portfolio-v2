dfcca6dc10501d497998a4f0e8e231cc131f3441
Colin Gillespie <ratchetandcolin@gmail.com>
2020-10-07 22:33:57 -0400

    Include back-to-top button in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { COLOURS } from 'styling';
 import styled from 'styled-components';
+import { ReactComponent as ChevronUpSVG } from 'assets/svgs/chevron-up.svg';
 
 const Container = styled.footer`
     padding: 2rem;
     background: ${COLOURS.bgMain};
     color: ${COLOURS.textMain};
+    margin: auto;
+`;
+
+const ArrowButton = styled.button`
+    background: none;
+    border: none;
+    svg {
+        stroke: ${COLOURS.textMain};
+        width: 64px;
+        height: 64px;
+        transition: stroke 300ms;
+    }
+    &:hover {
+        svg {
+            stroke: ${COLOURS.accent};
+        }
+    }
 `;
 
 const Footer = () => {
-    return <Container>Footer</Container>;
+    return (
+        <Container>
+            <ArrowButton>
+                <a href="#">
+                    <ChevronUpSVG />
+                </a>
+            </ArrowButton>
+        </Container>
+    );
 };
 
 export default Footer;
