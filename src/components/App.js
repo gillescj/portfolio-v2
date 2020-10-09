90ad9d02927a240f9c4bcf93d131fedaadd2c8d8
Colin Gillespie <ratchetandcolin@gmail.com>
2020-10-09 18:31:41 -0400

    Set up google analytics with react-ga

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { COLOURS } from 'styling';
 import Layout from 'components/Layout';
 import Hero from 'components/Hero';
 import ProjectsList from './ProjectsList';
 import Contact from './Contact';
+import ReactGA from 'react-ga';
 
 const Container = styled.div`
     background: ${COLOURS.bgMain};
     color: ${COLOURS.textMain};
 `;
+
+ReactGA.initialize('UA-179273123-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
     return (
